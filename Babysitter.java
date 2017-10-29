01f13fd7f02da9a28db9e7ba73b11f8a561dfc7c
Shayne <srouse.231@gmail.com>
2017-10-29 10:03:45 -0400

    end and bed time getters added

--- Babysitter.java
+++ Babysitter.java
@@ -3,12 +3,22 @@ package babysitter;
 public class Babysitter {
 	
 	private int startTime = 17;
-	
-	
+	private int endTime = 4;
+	private int bedTime = 10;
 	
 	public int getStartTime() {
 		
 		return startTime;
+	}
+
+	public int getEndTime() {
+		
+		return endTime;
+	}
+
+	public int getBedTime() {
+		
+		return bedTime;
 	}
 
 }
--- BabysitterTest.java
+++ BabysitterTest.java
@@ -7,9 +7,20 @@ import org.junit.jupiter.api.Test;
 class BabysitterTest {
     
 	Babysitter testSitter = new Babysitter();
+	
 	@Test
 	public void shouldReturnStartTime() {
 		assertEquals(17, testSitter.getStartTime());
+	}
+	
+	@Test
+	public void shouldReturnEndTime() {
+		assertEquals(4, testSitter.getEndTime());
+	}
+	
+	@Test
+	public void shouldReturnBedTime() {
+		assertEquals(10, testSitter.getBedTime());
 	}
 
 }
