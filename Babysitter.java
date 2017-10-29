907ebb1e24c422598289f9d1be76aa055f2e1225
Shayne <srouse.231@gmail.com>
2017-10-29 10:23:04 -0400

    end,bedTime setters

--- Babysitter.java
+++ Babysitter.java
@@ -3,21 +3,32 @@ package babysitter;
 public class Babysitter {
 	
 	private int startTime;
-	private int endTime = 4;
-	private int bedTime = 10;
+	private int endTime;
+	private int bedTime;
 	
 	public void setStartTime(int startTime) {
 		
 		this.startTime = startTime;
 	}
+	
 	public int getStartTime() {
 		
 		return startTime;
 	}
-
+    
+	public void setEndTime(int endTime) {
+		
+		this.endTime = endTime;
+	}
+	
 	public int getEndTime() {
 		
 		return endTime;
+	}
+	
+	public void setBedTime(int bedTime) {
+		
+		this.bedTime = bedTime;
 	}
 
 	public int getBedTime() {
